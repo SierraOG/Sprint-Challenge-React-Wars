1d75f28c91c8813e9d0ec3275c616fa0a1595ffa
SierraOG <sierrao@utexas.edu>
2019-07-19 12:08:32 -0500

    added starship info and more styling

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -14,12 +14,17 @@ const Card = styled.div`
 
 const Name = styled.h1`
     width: 90%;
-    margin: auto;
+    margin: 10px auto;
     border-bottom: 1.5px dashed grey;
+`
+const SubTitle = styled.p`
+    color: dimgrey;
 `
 
 function CharacterCard({character}) {
     const [hometown, setHometown] = useState(character.homeworld)
+    const [starships, setStarships] = useState('')
+    const starshipsArray = character.starships
     useEffect(() =>{
         axios.get(hometown)
         .then( data => {
@@ -28,15 +33,23 @@ function CharacterCard({character}) {
         .catch(error=>{
           console.log('error')
         })
+        starshipsArray.forEach((ship) => {
+            axios.get(ship)
+            .then( data => {
+                setStarships(data.data.name)
+            })
+            .catch(error => {console.log('error')})
+        })
     }, [])
     return(
     <Card>
         <Name>{character.name}</Name>
-        <p>Home planet: {hometown}</p>
+        <SubTitle>{hometown}</SubTitle>
         <p>Description: </p>
         <p>Skin color: {character.skin_color}</p>
         <p>Height: {character.height} cm</p>
         <p>Mass: {character.mass} kg</p>
+        <p>{starships !== '' ? `Starships: ${starships}` : ''}</p>
     </Card>
     )
 }
